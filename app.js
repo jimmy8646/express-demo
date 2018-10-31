f1a4d20cdf15c158d62758995dad49cd3ee513c5
jimmy8646 <yujiechueh@gmail.com>
2018-11-01 00:40:04 +0800

    refactor the code

--- app.js
+++ app.js
@@ -31,10 +31,7 @@ app.post("/api/courses", (req, res) => {
   const {
     error
   } = validateCourse(req.body)
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -46,26 +43,34 @@ app.post("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("course id not fund");
+  if (!course) return res.status(404).send("course id not fund");
   res.send(course);
 });
 
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("course id not fund");
+  if (!course) return res.status(404).send("course id not fund");
 
   const {
     error
   } = validateCourse(req.body)
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
+
 
   course.name = req.body.name
   res.send(course)
 
 });
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("course id not fund");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1)
+
+  res.send(course)
+})
 
 const port = process.env.PORT || 3000;
 
