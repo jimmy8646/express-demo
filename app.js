639fad6630fe46aa92ead5f8b6c6745c81507b19
jimmy8646 <yujiechueh@gmail.com>
2018-11-10 22:19:26 +0800

    feat: courses api demo finished

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ const debug = require('debug')('app:startup');
 const morgan = require("morgan");
 const helmet = require("helmet");
 const Joi = require("joi");
-const logger = require("./logger");
-const auth = require("./auth");
+const logger = require("./middleware/logger");
 const express = require("express");
 const app = express();
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 
-app.set('view engine','pug')
-app.set('views','./views')
+
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.use(helmet());
+app.use('/api/courses', courses)
+app.use('/', home)
 
-
-if(app.get('env')==='development'){
+if (app.get('env') === 'development') {
   debug('use Morgan')
   app.use(morgan("tiny"));
 }
@@ -24,79 +29,8 @@ if(app.get('env')==='development'){
 app.use(logger);
 app.use(auth);
 
-const courses = [
-  {
-    id: 1,
-    name: "courses1"
-  },
-  {
-    id: 2,
-    name: "courses2"
-  },
-  {
-    id: 3,
-    name: "courses3"
-  }
-];
-
-app.get("/", (req, res) => {
-  res.render('index',{title:'My Express App',msg:'hello~'})
-});
-
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
-
-app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("course id not fund");
-  res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("course id not fund");
-
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("course id not fund");
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-});
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`監聽 ${port}`);
 });
-
-const validateCourse = course => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-  return Joi.validate(course, schema);
-};
--- auth.js
+++
@@ -1,6 +0,0 @@
-const auth = (req,res,next) => {
-  console.log('Auth...')
-  next()
-}
-
-module.exports = auth
